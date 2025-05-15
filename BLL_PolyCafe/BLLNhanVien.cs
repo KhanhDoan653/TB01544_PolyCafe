36158b653a1e12d2c38e381ffec6e21250f9260e
KhanhDoanDev <dqk2092008@gmail.com>
2025-05-15 16:56:06 +0700

    Comit

---
+++ BLL_PolyCafe/BLLNhanVien.cs
@@ -0,0 +1,23 @@
+﻿using DAL_PolyCafe;
+using DTO_PolyCafe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_PolyCafe
+{
+    public class BLLNhanVien
+    {
+        DALNhanVien dalNhanVien = new DALNhanVien();
+        public NhanVien DangNhap(string email, string password)
+        {
+            if(string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            return dalNhanVien.getNhanVien(email, password);
+        }
+    }
+}
